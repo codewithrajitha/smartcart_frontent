9c0fc62fb0c70fd52c7773f0810aaccda5d9d1ae
codewithrajitha <rajitharajan2255@gmail.com>
2026-06-09 15:08:52 +0530

    Fix register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/login";
-import Register from "./pages/register";
+import Register from "./pages/Register";
 import Home from './pages/Home'
 import "./App.css";
 import UserViewProducts from './components/viewproducts'
